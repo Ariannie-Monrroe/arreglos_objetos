9f65c373d3c08e9ae01c2dcd151a021b01baa554
Ari Monrroe <ariannie.monrroe@gmail.com>
2024-12-15 18:17:10 -0300

    agregue img al array

--- assets/js/data/propiedades.js
+++ assets/js/data/propiedades.js
@@ -13,7 +13,7 @@ const propiedades_alquiler = [
     },
     {
         nombre: 'Casa Familiar',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6a9BHPCUoayTgVvUktUBIxOsox0o99dQaFQ&s',
         descripcion: 'Casa ideal para familias grandes con jardín privado.',
         ubicacion: 'Viña del Mar, Chile',
         habitaciones: 4,
@@ -24,7 +24,7 @@ const propiedades_alquiler = [
     },
     {
         nombre: 'Estudio Moderno',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmVZNb8M3MP93002-Pyyx865kmm9NKDKxBtA&s',
         descripcion: 'Estudio moderno cerca del metro.',
         ubicacion: 'Providencia, Chile',
         habitaciones: 1,
@@ -35,7 +35,7 @@ const propiedades_alquiler = [
     },
     {
         nombre: 'Cabaña Rústica',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvYEyldocxMvLMBDqWKk2MxC-SU8tqQZOTYg&s',
         descripcion: 'Cabaña acogedora en la montaña.',
         ubicacion: 'La Serena, Chile',
         habitaciones: 2,
@@ -50,7 +50,7 @@ const propiedades_alquiler = [
 const propiedades_venta = [
     {
         nombre: 'Casa de Lujo',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://img.freepik.com/foto-gratis/villa-lujo-piscina-espectacular-diseno-contemporaneo-arte-digital-bienes-raices-hogar-casa-propiedad-ge_1258-150749.jpg',
         descripcion: 'Casa de lujo con piscina y amplios jardines.',
         ubicacion: 'Las Condes, Chile',
         habitaciones: 5,
@@ -61,7 +61,7 @@ const propiedades_venta = [
     },
     {
         nombre: 'Departamento Urbano',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://images.homify.com/v1453219822/p/photo/image/1255680/Arquiteta_Camila_Castilho-84.jpg',
         descripcion: 'Departamento con vista al parque.',
         ubicacion: 'Ñuñoa, Chile',
         habitaciones: 2,
@@ -72,7 +72,7 @@ const propiedades_venta = [
     },
     {
         nombre: 'Parcela de Agrado',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://planoscasastop.com/wp-content/uploads/2021/08/wc-planos-casas-renders-rio-10.jpg',
         descripcion: 'Amplia parcela con árboles frutales y terreno para cultivo.',
         ubicacion: 'Valle del Elqui, Chile',
         habitaciones: 3,
@@ -83,7 +83,7 @@ const propiedades_venta = [
     },
     {
         nombre: 'Cabaña Playera',
-        src: 'https://theconcretehome.com/wp-content/uploads/2024/02/portada-galeria-tres-cantos.webp',
+        src: 'https://static.vecteezy.com/system/resources/thumbnails/023/307/472/small/ai-generative-exterior-of-modern-luxury-house-with-garden-and-beautiful-sky-photo.jpg',
         descripcion: 'Cabaña con acceso directo a la playa.',
         ubicacion: 'Pichilemu, Chile',
         habitaciones: 2,
